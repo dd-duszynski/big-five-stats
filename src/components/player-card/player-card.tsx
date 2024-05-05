03d2154810269031298d40bd9f0a96e8aac2b6a1
dd-duszynski <dd.duszynski@gmail.com>
2024-05-05 21:57:38 +0200

    feat: player card ui improvement

--- src/components/player-card/player-card.tsx
+++ src/components/player-card/player-card.tsx
@@ -1,5 +1,6 @@
 import { TeamSquadPlayer } from '@/models/TeamSquad.model';
 import Image from 'next/image';
+import Link from 'next/link';
 
 interface PlayerCardProps {
   player: TeamSquadPlayer;
@@ -7,22 +8,29 @@ interface PlayerCardProps {
 
 export const PlayerCard = ({ player }: PlayerCardProps) => {
   return (
-    <div className="flex h-[200px] w-[170px] flex-col items-center justify-center rounded-md border border-purple-300">
+    <div className="relative flex h-[200px] w-[170px] flex-col items-center justify-center rounded-md border border-purple-300">
       <div className="relative flex flex-col">
         <Image
           src={player.photo}
           alt={player.name}
           width={80}
           height={80}
-          className="rounded-lg "
+          className="rounded-lg"
         />
         <div className="border-purple absolute left-[-6px] top-0 rounded-full border bg-gray-200  p-1 text-xs">
           <span>{player.number}</span>
         </div>
       </div>
-      <p>{player.name}</p>
+      <Link
+        className="hover:underline"
+        href={`/player/${player.id}`}
+      >
+        <p>{player.name}</p>
+      </Link>
       <p>Age: {player.age}</p>
-      <p>{player.position}</p>
+      <div className="absolute bottom-0 flex w-full justify-center rounded-b-md bg-slate-400">
+        <p>{player.position}</p>
+      </div>
     </div>
   );
 };
